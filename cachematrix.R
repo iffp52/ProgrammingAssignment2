7ccda02f12b54eea483a2e60a1993982c7853dc9
Asis Datta <adatta2@roadrunner.com>
2016-06-18 12:44:03 -0700

    Create a matrix and then compute the inverse of it
    The function creation of the matrix and then the function to compute its
    inverse demonstrates the use of caching to speed up processing of large
    datasets.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
+## Set the value of the matrix
+## Get the value of the matrix
+## Set the inverse of the matrix
+## Get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inversem <- NULL
+        set <- function(y) {
+                x <<- y
+                inversem <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inversem <<- inverse
+        getinv <- function() inversem
+        list(set = set, get = get, 
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix"
+## returned by makeCacheMatrix. If the inverse has already been calculated
+## (and the matrix has not changed), then the cachesolve should retrieve
+## the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inversem <- x$getinv()
+        if(!is.null(inversem)) {
+                message("getting cached data")
+                return(inversem)
+        }
+        data <- x$get()
+        inversem <- solve(data, ...)
+        x$setinv(inversem)
+        inversem
 }
